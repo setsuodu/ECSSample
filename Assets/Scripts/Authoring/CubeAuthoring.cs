ebd50481ecf08a83554b45b6848f35af43b9ae4a
Xue <setsuodu@users.noreply.github.com>
2025-01-26 16:05:37 +0800

    完成移动、旋转 System

---
+++ Assets/Scripts/Authoring/CubeAuthoring.cs
@@ -0,0 +1,23 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class CubeAuthoring : MonoBehaviour
+{
+    public int speed;
+}
+
+public struct Cube : IComponentData
+{
+    public int speed;
+}
+
+public class CubeBaker : Baker<CubeAuthoring>
+{
+    public override void Bake(CubeAuthoring authoring)
+    {
+        //AddComponent<Cube>(); //远古写法
+
+        var entity = GetEntity(TransformUsageFlags.Dynamic);
+        AddComponent(entity, new Cube { speed = authoring.speed }); //ComponentData中所有属性，都要添加
+    }
+}
